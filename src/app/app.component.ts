adec71636de2d835eb06ef53fae086f1e6230eae
Mandy Blaschke <post@mandy-blaschke.de>
2020-11-10 16:45:29 +0100

    added: count score by difficulty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  game: boolean;
-  gameEnded = false;
+  activeGame = false;
+  finishedGame = false;
+  playDuration = 90000;
 
   moles: Mole[] = [
     {visible: false},
@@ -22,55 +23,92 @@ export class AppComponent implements OnInit {
     {visible: false}
   ];
 
-  counter = 0;
+  score = 0;
+
+  firstThird = false;
+  secondThird = false;
+  lastThird = false;
 
   ngOnInit(): void {
   }
 
+  private gameplayTimer(): void {
+    this.activeGame = true;
+    this.firstThird = true;
+
+    setTimeout(() => {
+      this.playingGame();
+      this.firstThird = false;
+      this.secondThird = true;
+    }, 10000);
+
+    setTimeout(() => {
+      this.secondThird = false;
+      this.lastThird = true;
+      this.playingGame();
+    }, 20000);
+
+    setTimeout(() => {
+      this.endGame();
+      this.lastThird = true;
+    }, this.playDuration);
+  }
+
   startGame(): void {
     this.gameplayTimer();
-    this.gameplay();
+    this.playingGame();
   }
 
-  gameplayTimer(): void {
-    this.game = true;
-    setTimeout(() => {
-      console.log('Spiel beendet');
-      this.moles.forEach( (mole) => mole.visible = false);
-      this.game = false;
-      this.gameEnded = true;
-    }, 30000);
+  private endGame(): void {
+    this.moles.forEach((mole) => mole.visible = false);
+    this.activeGame = false;
+    this.finishedGame = true;
   }
 
-  gameplay(): void {
-    if (this.game) {
+  private playingGame(): void {
+    if (this.activeGame) {
       setTimeout(() => {
         this.showAMole();
       }, 1000);
     }
   }
 
-  showAMole(): void {
+  private showAMole(): void {
     const randomMole = this.moles[Math.floor(Math.random() * this.moles.length)];
     setTimeout(() => {
       randomMole.visible = true;
     }, 1000);
     setTimeout(() => {
       randomMole.visible = false;
-    }, 2000);
-    this.gameplay();
+    }, 2300);
+    this.playingGame();
   }
 
 
-  scores(mole): void {
-    if (this.game && mole.visible) {
-      this.counter++;
+  scores(mole: Mole): void {
+    if (this.activeGame && mole.visible && this.firstThird) {
+      this.score++;
       mole.visible = false;
-    } else if (this.game && mole.visible === false) {
-      this.counter--;
+    } else if (this.activeGame && mole.visible === false && this.firstThird) {
+      this.score--;
+    }
+
+    if (this.activeGame && mole.visible && this.secondThird) {
+      this.score = this.score + 5;
+      mole.visible = false;
+    } else if (this.activeGame && mole.visible === false && this.secondThird) {
+      this.score = this.score - 5;
+    }
+
+    if (this.activeGame && mole.visible && this.lastThird) {
+      this.score = this.score + 10;
+      mole.visible = false;
+    } else if (this.activeGame && mole.visible === false && this.lastThird) {
+      this.score = this.score - 10;
     }
   }
 }
+
 
 export interface Mole {
   visible: boolean;
